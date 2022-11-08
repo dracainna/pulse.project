38ad11df593c812f83533332d32108d6bfce2df6
dracainna <annasafonova.info@gmail.com>
2022-11-08 18:52:39 +0300

    feat: create backend

--- dist/js/script.js
+++ dist/js/script.js
@@ -128,5 +128,35 @@ $(document).ready(function(){
             i.setAttribute('height', iframeHeight * iframeProportion); // устанавливаем высоту нашим Iframe
         });
     });
+
+    
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    if (!$(this).valid()) {
+      return;
+    };
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
   });
-  
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 900) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  });
