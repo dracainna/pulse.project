a1a147aa4285580af9f6e7fafae015ee0e57fc5e
dracainna <annasafonova.info@gmail.com>
2022-11-08 19:35:25 +0300

    Update smart.php

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -8,25 +8,18 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP('annasafonova.info@gmail.com');                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'annasafonova.info@gmail.com';                 // Наш логин
-$mail->Password = 'ovheqliniczvyvka';                           // Наш пароль от ящика
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                    // TCP port to connect to
+// $mail->isSMTP('пример почты');
+// $mail->Host = 'smtp.gmail.com'; 
+// $mail->SMTPAuth = true;
+// $mail->Username = 'пример почты';
+// $mail->Password = 'ovheqliniczvyvka';
+// $mail->SMTPSecure = 'ssl'; 
+// $mail->Port = 465;                            
  
-$mail->setFrom('annasafonova.info@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('bluerrionixri@gmail.com');     // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+// $mail->setFrom('пример почты', 'Pulse');  
+// $mail->addAddress('bluerrionixri@gmail.com');  
+
+$mail->isHTML(true); 
 
 $mail->Subject = 'Данные';
 $mail->Body    = '
